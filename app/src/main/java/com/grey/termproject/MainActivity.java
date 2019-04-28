d3162370bdfd517a206e3616b0a2d19b314e1fdb
Grey Ruessler <greyruessler@gmail.com>
2019-04-27 23:41:10 -0500

    implemented basic fraction + FAB

--- app/src/main/java/com/grey/termproject/MainActivity.java
+++ app/src/main/java/com/grey/termproject/MainActivity.java
@@ -1,13 +1,26 @@
 package com.grey.termproject;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ListFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (findViewById(R.id.fragment_container) != null) {
+            if (savedInstanceState != null)
+                return;
+            ListFragment firstFrag = new ListFragment();
+            firstFrag.setArguments(getIntent().getExtras());
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container, firstFrag).commit();
+        }
+    }
+
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
